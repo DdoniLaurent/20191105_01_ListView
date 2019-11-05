165a418168045adbf76df0b8908bc5a2c7ca47d1
YongMinSun <ddoni1010@gmail.com>
2019-11-05 21:59:37 +0900

    어댑터의 getView에서 상황에 맞는 내용 출력

--- app/src/main/java/com/tioeun/a20191105_01_listview/adapters/PostAdapter.kt
+++ app/src/main/java/com/tioeun/a20191105_01_listview/adapters/PostAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.tioeun.a20191105_01_listview.R
 import com.tioeun.a20191105_01_listview.datas.PostData
 
@@ -19,6 +20,14 @@ class PostAdapter(context:Context, res:Int, list:ArrayList<PostData>) : ArrayAda
         if(row == null){
             row = inf.inflate(R.layout.post_list_item, null)
         }
+
+        var postData = mList.get(position)
+
+        var titleTxt = row?.findViewById<TextView>(R.id.titleTxt)
+        var contentTxt = row?.findViewById<TextView>(R.id.contentTxt)
+
+        titleTxt?.text = postData.title
+        contentTxt?.text = postData.content
 
         return row!!
     }
