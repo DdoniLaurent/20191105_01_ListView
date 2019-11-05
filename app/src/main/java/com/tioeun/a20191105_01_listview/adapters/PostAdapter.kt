84630b351107ec4a44b361a4758880f475ccb4f2
YongMinSun <ddoni1010@gmail.com>
2019-11-05 21:52:19 +0900

    어댑터 클래스 기초 구조 정의

---
+++ app/src/main/java/com/tioeun/a20191105_01_listview/adapters/PostAdapter.kt
@@ -0,0 +1,25 @@
+package com.tioeun.a20191105_01_listview.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.tioeun.a20191105_01_listview.R
+import com.tioeun.a20191105_01_listview.datas.PostData
+
+class PostAdapter(context:Context, res:Int, list:ArrayList<PostData>) : ArrayAdapter<PostData>(context, res, list) {
+
+    var mContext = context
+    var mList = list
+    var inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var row = convertView
+        if(row == null){
+            row = inf.inflate(R.layout.post_list_item, null)
+        }
+
+        return row!!
+    }
+}
