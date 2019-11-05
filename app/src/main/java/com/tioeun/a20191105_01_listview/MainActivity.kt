2cfa58f6ce1704a95d061da5f60cf321d23dfee4
YongMinSun <ddoni1010@gmail.com>
2019-11-05 20:49:04 +0900

    코틀린 생성자 활용 연습  - 클래스 이름 옆에 기본 생성자 지정   - init을 이용해서 기본 생성자로 받아온 변수를 초기값 세팅   - 보조 생성자 만들기 :this(기본생성자전달재료들)

--- app/src/main/java/com/tioeun/a20191105_01_listview/MainActivity.kt
+++ app/src/main/java/com/tioeun/a20191105_01_listview/MainActivity.kt
@@ -2,11 +2,52 @@ package com.tioeun.a20191105_01_listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tioeun.a20191105_01_listview.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+    
+    var roomList = ArrayList<RoomData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addRooms()
+    }
+
+    //Ex. 두 숫자(인자)의 합계를 리턴(Int)해주는 함수 생성.
+//    fun sum(num1:Int, num2:Int) : Int {
+//        val result = num1 + num2
+//        return result
+//    }
+    fun sun(num1:Int, num2:Int) = num1 + num2
+
+
+    fun addRooms(){
+
+        var room1 = RoomData()
+        room1.price = 8000
+        room1.address = "마포구 서교동"
+        room1.floor = 1
+        roomList.add(room1)
+
+        var room2 = RoomData()
+        room2.price = 28500
+        room2.address = "마포구 서교동"
+        room2.floor = 3
+        roomList.add(room2)
+
+        var room3 = RoomData()
+        room3.price = 12000
+        room3.address = "마포구 성산동"
+        room3.floor = 5
+        roomList.add(room3)
+
+        var room4 = RoomData(1200, "마포구 망원1동", 3)
+        roomList.add(room4)
+
+        roomList.add(RoomData(1500, "마포구 망원동", 2))
+
+        roomList.add(RoomData("은평구 불광동"))
     }
 }
